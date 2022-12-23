3af9b56cb577e4487338b363e6a2b5913d89273b
Libing Chen <libing.chen@gmail.com>
2022-12-23 23:05:16 +0800

    feat: add JedisConnectionFactory bean

--- src/main/java/org/mvnsearch/redis/demo/RedisDemoApplication.java
+++ src/main/java/org/mvnsearch/redis/demo/RedisDemoApplication.java
@@ -1,12 +1,25 @@
 package org.mvnsearch.redis.demo;
 
+import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @SpringBootApplication
+@EnableRedisDocumentRepositories
 public class RedisDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedisDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RedisDemoApplication.class, args);
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(RedisProperties redisProperties) {
+        final RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
+        configuration.setDatabase(redisProperties.getDatabase());
+        return new JedisConnectionFactory(configuration);
+    }
 }
