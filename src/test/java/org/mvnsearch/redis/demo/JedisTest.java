39157f042c26243a4a4eea4f61381156cb68717d
Libing Chen <libing.chen@gmail.com>
2023-09-04 21:40:00 +0800

    test: add @RemoteAccess

--- src/test/java/org/mvnsearch/redis/demo/JedisTest.java
+++ src/test/java/org/mvnsearch/redis/demo/JedisTest.java
@@ -3,14 +3,16 @@ package org.mvnsearch.redis.demo;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mvnsearch.microservices.annotator.RemoteAccess;
 import redis.clients.jedis.Jedis;
 
 public class JedisTest {
+    @RemoteAccess("redis")
     private static Jedis jedis;
 
     @BeforeAll
     public static void setUp() {
-        jedis = new Jedis("localhost", 16379);
+        jedis = new Jedis("localhost", 6379);
     }
 
     @AfterAll
@@ -21,6 +23,7 @@ public class JedisTest {
     @Test
     public void testOperation() {
         jedis.set("nick", "jackie");
-        System.out.println(jedis.get("nick"));
+        final String nick = jedis.get("nick");
+        System.out.println(nick);
     }
 }
